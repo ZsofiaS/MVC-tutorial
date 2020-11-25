3f4d2413c48f5b0c657a914d20d791aed91e0d20
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-11-25 14:51:32 +0000

    WebApi is now returning restaurants data when Get request sent

--- OdeToFood.Web/App_Start/ContainerConfig.cs
+++ OdeToFood.Web/App_Start/ContainerConfig.cs
@@ -1,10 +1,12 @@
 ﻿using Autofac;
 using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
 using OdeToFood.Data.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace OdeToFood.Web
@@ -12,17 +14,19 @@ namespace OdeToFood.Web
     public class ContainerConfig
     {
         // define a method that will be invoked in Global.asax at app start
-        internal static void RegisterContainer()
+        internal static void RegisterContainer(HttpConfiguration httpConfiguration)
         {
             var builder = new ContainerBuilder();
             // register controllers with Autofac
             builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterApiControllers(typeof(MvcApplication).Assembly);
             builder.RegisterType<InMemoryRestaurantData>()
                 .As<IRestaurantData>()
                 .SingleInstance();
 
             var container = builder.Build(); // building the container
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            httpConfiguration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
         }
     }
 }
